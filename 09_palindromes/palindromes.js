5ddf13cae7887a6888dc033cc211c72771f6d0e1
Dimablo <[dimaarias@gmail.com]>
2025-02-19 20:13:18 -0300

    pali 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+const normalizedStr = str.toLowerCase();
+const reversedStr = normalizedStr.split(``).reverse().join(``);
+return normalizedStr === reversedStr;
 };
+
+const palindromesReversed = palindromes(`racecar!`);
+console.log(palindromesReversed);
 
 // Do not edit below this line
 module.exports = palindromes;
