0de811f2b014a5a7e952bed10eea97ccac86bc99
Dimablo <[dimaarias@gmail.com]>
2025-02-19 01:39:29 -0300

    sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return 'ERROR'; // Cheking for the ERROR
+    }
+
+    if (a > b) {
+        [a, b] = [b, a]; 
+    }  // Invert a and b
+    
+    let sum = 0;
+    for (let i = a; i <= b; i++) {   //Making the loop to make the 
+        sum += i;                    //the loop for the operation
+    }
+    
+    return sum;
 
 };
 
+const result = sumAll(10, [90, 1]) //calling the function
 // Do not edit below this line
 module.exports = sumAll;
