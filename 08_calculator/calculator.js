5f110a2a1e7784d14a67fbb55f2eeabf7adb1367
Dimablo <[dimaarias@gmail.com]>
2025-02-19 13:16:23 -0300

    calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+ return arr.reduce((total, current) => total + current, 0); 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+return arr.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+let product =1;
+for (let i =n; i > 0; i--) {
+  product *= i;
+}
+return product;
+}; 
+
+const addResult = add();
+const subtractResult = subtract();
+const sumResult = sum([0]);
+const multiplyResult = multiply([0]);
+const powerResult = power();
+const factorialResult = factorial(10);
+console.log(addResult);
+console.log(subtractResult)
+console.log(sumResult);
+console.log(multiplyResult);
+console.log(powerResult);
+console.log(factorialResult);
 
 // Do not edit below this line
 module.exports = {
