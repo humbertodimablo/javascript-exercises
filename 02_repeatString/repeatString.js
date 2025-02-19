b79a13731d812485d81b07644598e191ede6460b
Dimablo <[dimaarias@gmail.com]>
2025-02-18 21:38:40 -0300

    repeatString complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = (string, num) =>{
+    if (num < 0) return `ERROR`
+    let result = ``;
+    for (let i = 0; i < num; i++){
+        result += string;
+    }
+return result;
 
 };
+
+const randomNumber = Math.floor(Math.random() * 1000);
+const repeated = repeatString(` `, 10);
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
