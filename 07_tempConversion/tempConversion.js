ada2f7793ec492a5504b0eb0a80459640a6d9d49
Dimablo <[dimaarias@gmail.com]>
2025-02-19 10:32:49 -0300

    tempConversion complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit -32) *(5/9);
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsious) {
+  const fahrenheit = (celsious *  (9 / 5)) + 32;
+  return Math.round(fahrenheit * 10) / 10
 };
 
+const celsiousConversion = (convertToCelsius());
+const fahrenheitConversion = (convertToFahrenheit(-10));
+
+console.log(celsiousConversion);
+console.log(fahrenheitConversion);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
