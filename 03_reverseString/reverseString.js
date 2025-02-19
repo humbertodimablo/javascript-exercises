20707450bced5a3f8c257e42f369881795908acb
Dimablo <[dimaarias@gmail.com]>
2025-02-18 21:52:29 -0300

    reverseString complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+return  string.split(``).reverse().join(``);
 
 };
+
+const reversed = reverseString(``);
 
 // Do not edit below this line
 module.exports = reverseString;
