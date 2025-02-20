b9e15edcf8430142bb28a8c7e837468cc95cf1a2
Dimablo <[dimaarias@gmail.com]>
2025-02-20 10:43:20 -0300

    fibnonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    const n = Number(input);
+    if (n < 0){
+        return `OOPS`
+    }
+
+    if (n === 1 || n === 2){
+        return 1; //to set the first two fibonacci number that are 1
+    }
+
+    let a = 1;
+    let b = 1;
+    let fib = 0; //instalize the Fibonacci number
+
+    for (let i = 3; i <= n; i++) {
+        fib = a + b;
+        a = b; 
+        b = fib;
+    }
+
+    return fib; 
 
 };
 
+const fibonacciResult = fibonacci(`8`);
+console.log(fibonacciResult);
 // Do not edit below this line
 module.exports = fibonacci;
