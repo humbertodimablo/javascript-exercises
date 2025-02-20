2b0f9a5e336d7e816f51cd19867b4901c5eb69b1
Dimablo <[dimaarias@gmail.com]>
2025-02-20 11:57:49 -0300

    getTheTitles complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+return books.map(books => books.title)
 };
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+  const getTitles = getTheTitles(books)
+  console.log(getTitles);
 // Do not edit below this line
 module.exports = getTheTitles;
